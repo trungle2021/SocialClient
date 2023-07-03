df694a377c4aa7dc07c2203fbf7cb9f64d54ab25
trungle2021 <leanhtrung97@gmail.com>
2023-07-03 15:32:27 +0700

    Init Source Project

---
+++ src/main/java/com/example/socialclient/SocialClientApplication.java
@@ -0,0 +1,13 @@
+package com.example.socialclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocialClientApplication.class, args);
+    }
+
+}
